2d1ffddb9ea9f28b64a25199f18da3f549e7c5a8
Encentria <mactosh@gmail.com>
2025-09-21 19:09:49 +0800

    Update env.js

--- src/core/env.js
+++ src/core/env.js
@@ -123,18 +123,18 @@ const defaults = new Map(
     // primary doh upstream
     CF_DNS_RESOLVER_URL: {
       type: "string",
-      default: "https://dns11.quad9.net/dns-query",
+      default: "https://cloudflare-dns.com/dns-query",
     },
     // secondary doh upstream
     CF_DNS_RESOLVER_URL_2: {
       type: "string",
-      default: "https://freedns.controld.com/p2",
+      default: "https://dns.google/dns-query",
     },
     // upstream recursive rethinkdns resolver running on Fly.io
     MAX_DNS_RESOLVER_URL: {
       type: "string",
       // must always end with a trailing slash
-      default: "https://.com/",
+      default: "https://max.rethinkdns.com/",
     },
     // max doh request processing timeout some requests may have to wait
     // for blocklists to download before being responded to.
@@ -165,7 +165,7 @@ const defaults = new Map(
     // courtesy db-ip.com/db/download/ip-to-country-lite
     GEOIP_URL: {
       type: "string",
-      default: "",
+      default: "https://cfstore.rethinkdns.com/geoip/2022/1667349639157/",
     },
     // treat all blocklists as wildcards, this means
     // if abc.xyz.com is in any blocklist, then
