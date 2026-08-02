368ad4ac76537da76e9a7a29e00ef752806b6fd8
Encentria <mactosh@gmail.com>
2026-08-02 08:26:16 +0800

    Update env.js

--- src/core/env.js
+++ src/core/env.js
@@ -150,7 +150,7 @@ const defaults = new Map(
     MAX_DNS_RESOLVER_URL: {
       type: "string",
       // must always end with a trailing slash
-      default: "https://cloudflare-dns.com",
+      default: "https://max.rethinkdns.com/",
     },
     // max doh request processing timeout some requests may have to wait
     // for blocklists to download before being responded to.
